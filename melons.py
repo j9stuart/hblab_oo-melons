34aefe865436270543a56a647637a89c6c6cf9cd
Hackbright Student <no-reply@hackbrightacademy.com>
2016-10-06 17:59:41 -0700

    Created classes and subclasses with methods unique to the subclasses

---
+++ melons.py
@@ -0,0 +1,76 @@
+"""This file should have our order classes in it."""
+
+class AbstractMelonOrder(object):
+    """ """
+    def __init__(self, species, qty, order_type, tax):
+
+        self.species = species
+        self.qty = qty
+        self.shipped = False
+        self.order_type = order_type
+        self.tax = tax
+
+    def get_total(self):
+        """Calculate price."""
+
+        base_price = 5
+        if self.species.lower() == "christmas melon":
+            base_price = base_price * 1.5
+
+        
+        total = (1 + self.tax) * self.qty * base_price
+        return total
+
+    def mark_shipped(self):
+        """Set shipped to true."""
+
+        self.shipped = True
+
+
+class DomesticMelonOrder(AbstractMelonOrder):
+    """A domestic (in the US) melon order."""
+
+    def __init__(self,species, qty):
+        """Initialize melon order attributes"""
+        super(DomesticMelonOrder, self).__init__(species, qty,"domestic", 0.08)
+
+
+
+class InternationalMelonOrder(AbstractMelonOrder):
+    """An international (non-US) melon order."""
+
+    def __init__(self, species, qty, country_code):
+        """Initialize melon order attributes"""
+
+        super(InternationalMelonOrder, self).__init__(species, qty, "international", 0.17)
+        self.country_code = country_code
+
+    def get_total(self):
+        """Calculate price."""
+
+        total = super(InternationalMelonOrder, self).get_total()
+
+        if self.qty >= 10:
+            total = total + 3
+
+        return total
+        
+
+    def get_country_code(self):
+        """Return the country code."""
+
+        return self.country_code
+
+class GovernmentMelonOrder(AbstractMelonOrder):
+    """Orders without tax"""
+
+    def __init__(self,species, qty):
+        """Initialize melon order attributes"""
+        super(GovernmentMelonOrder, self).__init__(species, qty,"domestic", 0)
+        self.passed_inspection = False
+
+    def mark_inspection(self):
+        """ Setting inspetion to true """
+        self.passed_inspection = True
+
+
